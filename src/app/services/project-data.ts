424ad9777b1ed9b05123ed230cd6684ebdb6460a
Chris <capozzic1@gmail.com>
2017-11-01 13:12:57 +0900

    Updated project data

--- src/app/services/project-data.ts
+++ src/app/services/project-data.ts
@@ -1,28 +1,38 @@
 import { Project } from './project-data.ts';
 
 export const PROJECTS: Project[] = [
-  {id: 1, name: "An ecommerce front end landing page",
-skills: "HTML, JavaScript, Sass, jQuery",
-description: "This is a landing page I made for a client in the UK(rufflefordogs.com)",
-url:"rufflefordogs.herokuapp.com"},
-{id:2,
-name:"A blog made with React and Redux",
-skills:"React, Redux, Express, MongoDB, Passport authentication, Radium CSS, Node.js",
-description:"This is a MERN stack blog that contains features such as creating, updating, deleting, and reading posts. It also has an authentication system.",
-url:"reactreduxblogcc.herokuapp.com"},
-{id:4,
-name:"An English Name Generator",
-skills:"React, Express, CSS, Node.js",
-description:"This is a MERN stack blog that contains features such as creating, updating, deleting, and reading posts. It also has an authentication system.",
-url:"reactreduxblogcc.herokuapp.com"},
-{id:2,
-name:"A blog made with React and Redux",
-skills:"React, Redux, Express, MongoDB, Passport authentication, Radium CSS, Node.js",
-description:"This is a MERN stack blog that contains features such as creating, updating, deleting, and reading posts. It also has an authentication system.",
-url:"reactreduxblogcc.herokuapp.com"},
-{id:2,
-name:"A blog made with React and Redux",
-skills:"React, Redux, Express, MongoDB, Passport authentication, Radium CSS, Node.js",
-description:"This is a MERN stack blog that contains features such as creating, updating, deleting, and reading posts. It also has an authentication system.",
-url:"reactreduxblogcc.herokuapp.com"},
+  {
+    id: 1, name: "An ecommerce front end landing page",
+    skills: "HTML, JavaScript, Sass, jQuery",
+    description: "This is a landing page I made for a client in the UK(rufflefordogs.com)",
+    url: "rufflefordogs.herokuapp.com"
+  },
+  {
+    id: 2,
+    name: "A blog made with React and Redux",
+    skills: "React, Redux, Express, MongoDB, Passport authentication, Radium CSS, Node.js, Custom express API",
+    description: "This is a MERN stack blog that contains features such as creating, updating, deleting, and reading posts. It also has an authentication system.",
+    url: "reactreduxblogcc.herokuapp.com"
+  },
+  {
+    id: 3,
+    name: "An English Name Generator",
+    skills: "React, Express, CSS, Node.js, Names API",
+    description: "This is a web app that generates English names.",
+    url: "https://english-gen.herokuapp.com/"
+  },
+  {
+    id: 4,
+    name: "A neighborhood map with fun things to do",
+    skills: "jQuery, Google Maps API, HTML, CSS",
+    description: "This is a map that shows fun things to do in downtown Las Vegas",
+    url: "https://capozzic1.github.io/knockout_map/"
+  },
+  {
+    id: 2,
+    name: "A blog made with React and Redux",
+    skills: "React, Redux, Express, MongoDB, Passport authentication, Radium CSS, Node.js",
+    description: "This is a MERN stack blog that contains features such as creating, updating, deleting, and reading posts. It also has an authentication system.",
+    url: "reactreduxblogcc.herokuapp.com"
+  },
 ]
