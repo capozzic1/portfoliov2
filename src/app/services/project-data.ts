90aa3ca98e61a3676e73574059db1857a919ec70
capozzic1 <capozzic1@gmail.com>
2017-11-01 10:48:26 +0900

    Added in project service

---
+++ src/app/services/project-data.ts
@@ -0,0 +1,28 @@
+import { Project } from './project-data.ts';
+
+export const PROJECTS: Project[] = [
+  {id: 1, name: "An ecommerce front end landing page",
+skills: "HTML, JavaScript, Sass, jQuery",
+description: "This is a landing page I made for a client in the UK(rufflefordogs.com)",
+url:"rufflefordogs.herokuapp.com"},
+{id:2,
+name:"A blog made with React and Redux",
+skills:"React, Redux, Express, MongoDB, Passport authentication, Radium CSS, Node.js",
+description:"This is a MERN stack blog that contains features such as creating, updating, deleting, and reading posts. It also has an authentication system.",
+url:"reactreduxblogcc.herokuapp.com"},
+{id:4,
+name:"An English Name Generator",
+skills:"React, Express, CSS, Node.js",
+description:"This is a MERN stack blog that contains features such as creating, updating, deleting, and reading posts. It also has an authentication system.",
+url:"reactreduxblogcc.herokuapp.com"},
+{id:2,
+name:"A blog made with React and Redux",
+skills:"React, Redux, Express, MongoDB, Passport authentication, Radium CSS, Node.js",
+description:"This is a MERN stack blog that contains features such as creating, updating, deleting, and reading posts. It also has an authentication system.",
+url:"reactreduxblogcc.herokuapp.com"},
+{id:2,
+name:"A blog made with React and Redux",
+skills:"React, Redux, Express, MongoDB, Passport authentication, Radium CSS, Node.js",
+description:"This is a MERN stack blog that contains features such as creating, updating, deleting, and reading posts. It also has an authentication system.",
+url:"reactreduxblogcc.herokuapp.com"},
+]
---
+++ src/app/services/project.ts
@@ -0,0 +1,9 @@
+
+export class Project {
+  id: number,
+  name: string,
+  skills: string,
+  description: string,
+  url: any,
+
+}
